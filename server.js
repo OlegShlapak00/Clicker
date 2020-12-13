bbfa18f4a195e2040b238703fd41f6df3aeb92fb
OlegShlapak00 <oleg.shlapak.00@gmail.com>
2020-12-13 13:00:57 +0200

    try to deploy8

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + '/dist/KR1'));
+app.use(express.static(__dirname + '/dist/kr1'));
 
 app.get('/', function (req,res){
   res.sendFile(path.join(__dirname + '/dist/KR1/index.html'));
